c0f542fe4ba8106ab98009493d46883b171e95d0
woltemade <herald.smit@gmail.com>
2019-07-30 14:56:02 +0200

    move progress bar to chaininfo page

--- app/src/pages/chain/info/index.js
+++ app/src/pages/chain/info/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Layout, PageHeader } from "antd";
+import { Layout, PageHeader, Progress, Row, Col } from "antd";
 // const { Content } = Layout;
 
 export default class Info extends React.Component {
@@ -7,20 +7,70 @@ export default class Info extends React.Component {
     super(props);
     // Don't call this.setState() here!
     this.state = {
-      data: null
+      data: null,
+      chainNetwork: process.env.REACT_APP_BCOIN_NETWORK,
+      chainProgress: 0.0001,
     };
   }
 
   componentDidMount() {
-    fetch("http://localhost:3001/chain/info")
-      .then(response => response.json())
-      .then(data => this.setState({ data }));
+    this.getChainState();
+    this.timer = setInterval(() => this.getChainState(), 5000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  getChainState() {
+    if (this.chainIsInReadyState()) {
+      clearInterval(this.timer);
+    } else {
+      fetch("http://localhost:3001/chain/info")
+        .then(response => response.json())
+        .then(data =>
+          this.setState({
+            chainProgress: data.chain.progress,
+            chainNetwork: `[${data.network}] node`,
+            data
+          })
+        );
+    }
+  }
+  chainIsInReadyState() {
+    if (this.state.chainProgress < 1) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+  progressBar() {
+    return (
+      <Layout>
+        <Row>
+          <Col span={4} />
+          <Col span={16}>
+            <h2>Blockchain {this.state.chainNetwork} is busy syncing...</h2>
+            <Progress
+              strokeColor={{
+                "0%": "#108ee9",
+                "100%": "#87d068"
+              }}
+              percent={Math.round(this.state.chainProgress * 10000) / 100}
+            />
+          </Col>
+          <Col span={4} />
+        </Row>
+      </Layout>
+    );
   }
   render() {
+    let progress = this.progressBar();
     return (
       <div>
       <Layout>
         <PageHeader title="Chain Info" subTitle="" />
+        {progress}
           <div style={{ padding: 24, background: "#fff", minHeight: 360 }}>
             <pre>{JSON.stringify(this.state.data, null, 2)}</pre>
           </div>
--- app/src/pages/main.js
+++ app/src/pages/main.js
@@ -12,7 +12,7 @@ import WalletList from "./wallet/walletList";
 import CreateWallet from "./wallet/create";
 import ImportPublicKey from "./wallet/add";
 
-import { Layout, Menu, Icon, Progress, Row, Col } from "antd";
+import { Layout, Menu, Icon } from "antd";
 const { Sider } = Layout;
 const { SubMenu } = Menu;
 
@@ -23,8 +23,6 @@ export default class Main extends React.Component {
     this.state = {
       collapsed: false,
       pageTitle: "",
-      chainNetwork: "node",
-      chainProgress: 0.0001,
       network: process.env.REACT_APP_BCOIN_NETWORK
     };
   }
@@ -46,33 +44,13 @@ export default class Main extends React.Component {
     this.setState({ collapsed: !this.state.collapsed });
   };
 
-  busySyncingPage() {
-    return (
-      <Layout>
-        <Row>
-          <Col span={4} />
-          <Col span={16}>
-            <h2>Blockchain {this.state.chainNetwork} is busy syncing...</h2>
-            <Progress
-              strokeColor={{
-                "0%": "#108ee9",
-                "100%": "#87d068"
-              }}
-              percent={Math.round(this.state.chainProgress * 10000) / 100}
-            />
-          </Col>
-          <Col span={4} />
-        </Row>
-      </Layout>
-    );
-  }
-
   isRegtest() {
     if (this.state.network === "regtest") {
       return true;
     }
     return false;
   }
+
   homePage() {
     return (
       <Layout>
@@ -147,43 +125,9 @@ export default class Main extends React.Component {
       </Layout>
     );
   }
-  componentDidMount() {
-    // this.timer = setInterval(() => this.getChainState(), 5000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timer);
-  }
-
-  getChainState() {
-    if (this.chainIsInReadyState()) {
-      clearInterval(this.timer);
-    } else {
-      fetch("http://localhost:3001/chain/info")
-        .then(response => response.json())
-        .then(data =>
-          this.setState({
-            chainProgress: data.chain.progress,
-            chainNetwork: `[${data.network}] node`
-          })
-        );
-    }
-  }
-  chainIsInReadyState() {
-    if (this.state.chainProgress < 1) {
-      return false;
-    } else {
-      return true;
-    }
-  }
 
   render(props) {
-    let page;
-    if (this.chainIsInReadyState()) {
-      page = this.homePage();
-    } else {
-      page = this.homePage(); //this.busySyncingPage();
-    }
+    let page = this.homePage();
 
     return (
       <div className="App">
