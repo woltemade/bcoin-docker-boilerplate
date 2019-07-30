107f20f34b6df10c7011758d64e4d537a0bab733
woltemade <herald.smit@gmail.com>
2019-07-30 16:17:03 +0200

    fix add wallet functionality - assuming only xpub wil be supplied.

--- api/src/routes/wallet/inves.js
+++ api/src/routes/wallet/inves.js
@@ -1,42 +1,58 @@
-require('module-alias/register')
-var router = require('express').Router();
+require("module-alias/register");
+const uuidv4 = require("uuid/v4");
+const router = require("express").Router();
 
-const initBcoinNode = require('@/bcoin')
+const initBcoinNode = require("@/bcoin");
 
 function emptyVal(val) {
-    if (!val || val.trim() === '') {
-        return true;
-    }
+  if (!val || val.trim() === "") {
+    return true;
+  }
 }
-router.get('/', async function(req, res, next) {
-    const bcoin = await initBcoinNode()
-    let errMessage = 'No error';
-    const walletId = req.query.walletId || 'wa'; // inves user id 
-    const accountId = req.query.accountId || 'acc'; // user can have more than one wallet, name must be unique
-    const publicKey = req.query.publicKey || 'te'; //we need to check that it is not already added
-    // 1. check that all values are supplied
-    console.log(`accountid: ${accountId}`)
-    if (emptyVal(accountId)) {
-        errMessage = `AccountId must be supplied: ${accountId}`;
-        res.status(301).json({walletIfno: errMessage});
-    }
-    if (emptyVal(walletId)) {
-        errMessage = 'WalletId must be supplied.';
-        res.status(301).json({walletIfno: errMessage});
-    }
+
+router.get("/", async function(req, res, next) {
+  try {
+    const bcoin = await initBcoinNode();
+    let errMessage = "No error";
+
+    const publicKey = req.query.xpub; //we need to check that it is not already added
+    const walletId = uuidv4();
+    const accountId = "default";
+
     if (emptyVal(publicKey)) {
-        errMessage = 'PublicKey must be supplied.';
-        res.status(301).json({walletIfno: errMessage});
+      errMessage = "PublicKey must be supplied.";
+      res.status(301).json({ walletInfo: errMessage });
     }
-    // 2. check if user wallet exists
-    // 3. List accounts and check if public key is already added
-    // 4. Check that account id is not already in use.
-    // const wallet = await bcoin.walletClient.wallet(walletId);
-    // const walletIfno = await wallet.getInfo();
-    res.status(200).json({walletIfno: 'adding wallet....'});
+
+    let options = {
+      walletId: walletId,
+      accountId: accountId,
+      watchOnly: req.query.watchOnly || true,
+      accountKey: publicKey
+    };
+
+    const result = await bcoin.walletClient.createWallet(
+      options.walletId,
+      options
+    );
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      console.log(
+        "error in wallet create response:",
+        JSON.stringify(result, null, 4)
+      );
+      res.status(301).json({ error: result });
+    }
+  } catch (e) {
+    res.status(301).json(e);
+  }
+  // 2. check if user wallet exists
+  // 3. List accounts and check if public key is already added
+  // 4. Check that account id is not already in use.
+  // const wallet = await bcoin.walletClient.wallet(walletId);
+  // const walletInfo = await wallet.getInfo();
+  //
 });
 
 module.exports = router;
-
-
-
--- app/src/pages/wallet/inves.js
+++ app/src/pages/wallet/inves.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Layout, PageHeader } from "antd";
-// const { Content } = Layout;
+import { Layout, PageHeader, Row, Col, Input } from "antd";
+
+const { Search } = Input;
 
 export default class Info extends React.Component {
   constructor(props) {
@@ -11,21 +12,44 @@ export default class Info extends React.Component {
     };
   }
 
-  componentDidMount() {
-    fetch("http://localhost:3001/wallet/inves")
+  createWallet(xpub) {
+    console.log(xpub);
+    fetch(`http://localhost:3001/wallet/inves?xpub=${xpub}`)
       .then(response => response.json())
       .then(data => this.setState({ data }));
   }
+  
+  isObject(value) {
+    return value && typeof value === "object" && value.constructor === Object;
+  }
+
+
   render() {
+    let result;
+
+    if (this.isObject(this.state.data)) {
+      result = <pre>{JSON.stringify(this.state.data, null, 2)}</pre>;
+    }
+
     return (
-      <div>
       <Layout>
-        <PageHeader title="Inves Wallet Create Info" subTitle="" />
-          <div style={{ padding: 24, background: "#fff", minHeight: 360 }}>
-            <pre>{JSON.stringify(this.state.data, null, 2)}</pre>
-          </div>
+        <PageHeader title="Add a new wallet" subTitle="" />
+        <div style={{ padding: 24, background: "#fff", minHeight: 360 }}>
+          <Row>
+            <Col span={24}>
+              <Search
+                placeholder="Enter xpub key:"
+                enterButton="Create"
+                size="large"
+                onSearch={xpub => this.createWallet(xpub)}
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col span={24}>{result}</Col>
+          </Row>
+        </div>
       </Layout>
-      </div>
     );
   }
 }
